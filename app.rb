5ac64cd8137ca0b9013aa838d562dafefeba4dfc
korey ko <koreykendrick1@gmail.com>
2020-07-04 06:38:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do
-  @medicines = Medicine.all
+  get '/name' do
+  @ = Medicine.all
  
   erb :'medicines/index.html.erb'
 end
